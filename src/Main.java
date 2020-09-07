f9160b94cccedeabb3b79a44d41d1e5f6b93d40e
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-09-07 17:55:48 +0100

    Working reverseRecursive() and reverseIterative()

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,35 @@ public class Main
 {
     public static void main(String[] args)
     {
-        BinaryTreeNode root = new BinaryTreeNode(4,
-                new BinaryTreeNode(2,
-                        new BinaryTreeNode(1),
-                        new BinaryTreeNode(3)),
-                new BinaryTreeNode(7,
-                        new BinaryTreeNode(6,
-                                new BinaryTreeNode(8),
-                                null),
-                        new BinaryTreeNode(9))
+        BinaryTreeNode root = new BinaryTreeNode(
+                4,
+                new BinaryTreeNode(
+                        2,
+                        new BinaryTreeNode(
+                                1
+                        ),
+                        new BinaryTreeNode(
+                                3
+                        )
+                ),
+                new BinaryTreeNode(
+                        7,
+                        new BinaryTreeNode(
+                                6,
+                                new BinaryTreeNode(
+                                        8
+                                ),
+                                null
+                        ),
+                        new BinaryTreeNode(
+                                9
+                        )
+                )
         );
-
+        System.out.println();
+        root.reverseRecursive(root);
+        System.out.println();
+        root.reverseIterative(root);
         System.out.println();
     }
 }
